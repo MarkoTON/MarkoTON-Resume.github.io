77d921f54e5337406a352e64ad85a35cc9efa190
MarkoTON <MarkoTON@users.noreply.github.com>
2018-08-15 18:55:27 +0200

    Add skills and transition on #github-rep

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
 window.sr = ScrollReveal();
 sr.reveal('.navbar', {
-  duration: 2000,
+  duration: 1500,
   origin: 'top'
 });
 sr.reveal('.header', {
-  duration: 2000,
+  duration: 1500,
   origin: 'top'
 });
 sr.reveal('.col-md-7', {
-  duration: 2000,
+  duration: 1500,
   origin: 'right',
   distance: '300px'
 });
 sr.reveal('.col-md-5', {
-  duration: 2000,
+  duration: 1500,
   origin: 'left',
   distance: '300px'
 });
 sr.reveal('.my-career', {
-  duration: 2000,
+  duration: 1500,
   origin: 'bottom'
 });
 sr.reveal('#about', {
-  duration: 2000,
+  duration: 1500,
   origin: 'bottom',
   viewFactor: 0.4
 });
 sr.reveal('#experience .col-md-6', {
-  duration: 2000,
+  duration: 1500,
   origin: 'bottom',
   distance: '300px',
   viewFactor: 0.1
 });
 sr.reveal('#skills', {
-  duration: 2000,
+  duration: 1500,
   origin: 'top',
   distance: '300px',
   viewFactor: 0.1
 });
 sr.reveal('#education', {
-  duration: 2000,
+  duration: 1500,
   origin: 'bottom',
   distance: '300px',
   viewFactor: 0.1
 });
 sr.reveal('#activities', {
-  duration: 2000,
+  duration: 1500,
   origin: 'top',
   distance: '300px',
   viewFactor: 0.1
@@ -95,11 +95,12 @@ setInterval(arrUp, 1500);
 // addIcon();
 
 // When the user scrolls the page, execute myFunction 
+
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
