820c85d2dfebc555abdf6e41f5ca4b4aae554fd1
MarkoTON <MarkoTON@users.noreply.github.com>
2018-09-07 20:30:02 +0200

    Links and Text with JSON and AJAX

--- js/main.js
+++ js/main.js
@@ -50,11 +50,11 @@ sr.reveal('#activities', {
   distance: '300px',
   viewFactor: 0.1
 });
-sr.reveal('.col-md-2', {
+sr.reveal('#personInfoGithubRepository', {
   duration: 1000,
   viewFactor: 0.1,
   origin: 'bottom',
-}, 500);
+}, 1000);
 
 $(function () {
   // Smooth Scrolling
@@ -86,16 +86,6 @@ function arrUp() {
 arrUp();
 setInterval(arrUp, 1500);
 
-
-// function addIcon(){
-//   let icon;
-//   icon = document.getElementById("chess-icon");
-//   icon.innerHTML = "&#xf448;";
-// }
-// addIcon();
-
-// When the user scrolls the page, execute myFunction 
-
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
@@ -104,3 +94,89 @@ function myFunction() {
   let scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+// Stampanje podataka //
+function onClickPrint(){
+  // Hvatanje JSON-a i konvertovanje
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let response = JSON.parse(xhttp.responseText);
+      let infoCV = response[0];
+      let outputInfo;
+      let outputInfoSocNetw = '';
+      let outputInfoSocNetw2 = '';
+      let outputInfoGithubRepository = '';
+      
+      outputInfo = `
+      <h1 class="font-weight-bold">${infoCV.name}</h1>
+      <p class="lead">${infoCV.address}</p>
+      <p class="lead">${infoCV.phone}</p>
+      `;
+      
+      let socialNetworks = infoCV.socialNetworks;
+      for (let i = 0; i <= 2; i++) {
+        let obj = socialNetworks[i];
+        outputInfoSocNetw += `
+        <p class="lead">
+        <a target="_blank" href="${obj.address}">
+        <i class="${obj.icon}"></i> ${obj.name}
+        </a>
+        </p>
+        `;
+      }
+      
+      for (let i = 3; i <= 4; i++) {
+        let obj2 = socialNetworks[i];
+        outputInfoSocNetw2 += `
+        <p class="lead">
+        <a target="_blank" href="${obj2.address}">
+        <i class="${obj2.icon}"></i> ${obj2.name}
+        </a>
+        </p>
+        `;
+      }
+      
+      let githubRepository = infoCV.githubRepository;
+      for (let i = 0; i < githubRepository.length ; i++) {
+        let objGitHub = githubRepository[i];
+        outputInfoGithubRepository += `
+        <div class="col-sm-2 col-md-2">
+           <a href="${objGitHub.link}" target="_blank">
+              <img src="${objGitHub.imageLink}" alt="" class="rounded-square">
+           </a>
+        </div><!--  col-md-2 -->
+        `;
+      }
+      
+      document.getElementById('personInfo').innerHTML = outputInfo;
+      document.getElementById('personInfoSocNetw').innerHTML = outputInfoSocNetw;
+      document.getElementById('personInfoSocNetw2').innerHTML = outputInfoSocNetw2;
+      document.getElementById('personInfoGithubRepository').innerHTML = outputInfoGithubRepository;
+    }
+  };
+  
+  xhttp.open("GET", "json/info.json", true);
+  xhttp.send();
+}
+
+onClickPrint();
+
+// Stampanje Teksta //
+
+function printText(){
+  // Hvatanje JSON-a i konvertovanje
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let response = xhttp.responseText;
+      let text = response;
+      document.getElementById("textInfo").innerHTML = text;
+      console.log(text);
+    }
+  };
+  xhttp.open("GET", "json/data.txt", true);
+  xhttp.send();
+}
+
+printText();
