af124ba875f256c1bc1ba0cf524770f5e8b86f37
MarkoTON <MarkoTON@users.noreply.github.com>
2018-11-17 19:16:22 +0100

    New Button for Navbar

--- js/main.js
+++ js/main.js
@@ -1,61 +1,3 @@
-window.sr = ScrollReveal();
-sr.reveal('.navbar', {
-  duration: 1500,
-  origin: 'top'
-});
-sr.reveal('.header', {
-  duration: 1500,
-  origin: 'top'
-});
-sr.reveal('.col-md-7', {
-  duration: 1500,
-  origin: 'right',
-  distance: '300px'
-});
-sr.reveal('.col-md-5', {
-  duration: 1500,
-  origin: 'left',
-  distance: '300px'
-});
-sr.reveal('.my-career', {
-  duration: 1500,
-  origin: 'bottom'
-});
-sr.reveal('#about', {
-  duration: 1500,
-  origin: 'bottom',
-  viewFactor: 0.4
-});
-sr.reveal('#experience .col-md-6', {
-  duration: 1500,
-  origin: 'bottom',
-  distance: '300px',
-  viewFactor: 0.1
-});
-sr.reveal('#skills', {
-  duration: 1500,
-  origin: 'top',
-  distance: '300px',
-  viewFactor: 0.1
-});
-sr.reveal('#education', {
-  duration: 1500,
-  origin: 'bottom',
-  distance: '300px',
-  viewFactor: 0.1
-});
-sr.reveal('#activities', {
-  duration: 1500,
-  origin: 'top',
-  distance: '300px',
-  viewFactor: 0.1
-});
-sr.reveal('#personInfoGithubRepository', {
-  duration: 1000,
-  viewFactor: 0.1,
-  origin: 'bottom',
-}, 1000);
-
 $(function () {
   // Smooth Scrolling
   $('a[href*="#"]:not([href="#"])').click(function () {
@@ -180,3 +122,36 @@ function printText(){
 }
 
 printText();
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+// Menu Button X
+function myMenuBtn(x) {
+  x.classList.toggle("change");
+}
+
+function myMenuOverlay(overlayToogle){
+  overlayToogle.classList.toggle("overlay-toggle");
+}
+
+// Nav Open
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+// Nav Close
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
---
+++ js/scroll-main.js
@@ -0,0 +1,57 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+  duration: 1500,
+  origin: 'top'
+});
+sr.reveal('.header', {
+  duration: 1500,
+  origin: 'top'
+});
+sr.reveal('.col-md-7', {
+  duration: 1500,
+  origin: 'right',
+  distance: '300px'
+});
+sr.reveal('.col-md-5', {
+  duration: 1500,
+  origin: 'left',
+  distance: '300px'
+});
+sr.reveal('.my-career', {
+  duration: 1500,
+  origin: 'bottom'
+});
+sr.reveal('#about', {
+  duration: 1500,
+  origin: 'bottom',
+  viewFactor: 0.4
+});
+sr.reveal('#experience .col-md-6', {
+  duration: 1500,
+  origin: 'bottom',
+  distance: '300px',
+  viewFactor: 0.1
+});
+sr.reveal('#skills', {
+  duration: 1500,
+  origin: 'top',
+  distance: '300px',
+  viewFactor: 0.1
+});
+sr.reveal('#education', {
+  duration: 1500,
+  origin: 'bottom',
+  distance: '300px',
+  viewFactor: 0.1
+});
+sr.reveal('#activities', {
+  duration: 1500,
+  origin: 'top',
+  distance: '300px',
+  viewFactor: 0.1
+});
+sr.reveal('#personInfoGithubRepository', {
+  duration: 1000,
+  viewFactor: 0.1,
+  origin: 'bottom',
+}, 1000);
